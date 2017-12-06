86eff1b46232b816f196445f69afcf240618741e
Dawid <daw.belk@gmail.com>
2017-12-06 20:05:00 +0100

    last cuts

--- src/Blackjack/Runner/BlackJackRunner.java
+++ src/Blackjack/Runner/BlackJackRunner.java
@@ -1,28 +1,26 @@
 package Blackjack.Runner;
 
-import Blackjack.Dealer;
 import Blackjack.Gambler;
 import Blackjack.Player;
 
 public class BlackJackRunner {
     public static void main(String[] args) {
-        Player player = new Player();
 
         //1. Gambler first move
         Player.movesGambler();
 
         //2. Dealer first move
-        player.firstMoveDealer();
+        Player.firstMoveDealer();
 
         //3. Gambler decision to hit or stay
         Gambler gambler = new Gambler();
         gambler.decision();
 
         //4. Dealer next moves
-        player.moveDealer();
+        Player.moveDealer();
 
         //5. Comparison players
-        player.comparison();
+        Player.comparison();
 
     }
 }
